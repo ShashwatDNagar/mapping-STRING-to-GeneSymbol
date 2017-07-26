e948e6e744db385224e0535a80f039066b47b994
Shashwat Deepali Nagar <shashwat.d.nagar@gmail.com>
2017-07-25 20:03:20 -0500

    Adding provisions to accept command line arguments

--- STRING_to_GeneSymbol.R
+++ STRING_to_GeneSymbol.R
@@ -1,42 +1,30 @@
-#!/usr/bin/env R
+#!/usr/bin/env Rscript
 
 # Shashwat Deepali Nagar
-# Jordan Lab, Georgia Tech
+
 # Script to convert ESNP identifiers from STRING-DB to Official Gene Symbols and produce a file like this:
-#
-# Output file format:
-# GeneName1	GeneName2	neighborhood	fusion	cooccurence	coexpression	experimental	database	textmining	combined_score
-# ARF1GAP	ARF5	0	0	0	0	536	0	774	890
-# GGA3	ARF5	0	0	0	0	298	0	788	844
-# ASAP1	ARF5	0	0	0	0	240	0	790	833
-# GGA1	ARF5	0	0	0	0	683	0	760	920
-#
-# Input file format:
-# protein1	protein2	neighborhood	fusion	cooccurence	coexpression	experimental	database	textmining	combined_score
-# ENSP00000000233	ENSP00000158762	0	0	0	0	328	0	785	849
-# ENSP00000000233	ENSP00000237596	0	0	0	0	0	0	796	796
-# ENSP00000000233	ENSP00000245541	0	0	0	0	298	0	788	844
-# ENSP00000000233	ENSP00000263245	0	0	0	0	430	0	714	830
-#
-# Mapping file format:
-# STRING	Gene.name
-# ENSP00000397982	HLA-DBR1
-# ENSP00000343144	16/05/05
-# ENSP00000225928	17BHSDI
-# ENSP00000369600	214K23.2
-#
-# This can be obtained from filtering the mapping file available on http://www.string-db.org
-# using:
-# awk '{print substr($1,6,15), $2, $3, $4, $5}' string.aliases.txt > stringMapping.txt
-# grep "Blast_uniprot_gn" -i stringMapping.txt | awk 'BEGIN{OFS="\t"} {print $1, $2}' > mappingFile123.txt
+
+# Check README for details about input file.
+
+# Usage: ./STRING_to_GeneSymbol.R <Input File> <Mapping File> <Output File>
 
 
+# Reading in command-line arguments
+args <- commandArgs(TRUE)
+inputFile <- args[1]
+mappingFileLoc <- args[2]
+outputFileLoc <- args[3]
 
-mapString2genesymbol <- function(){
+# Check if arguments are provided
+if(length(args) < 3) {
+  stop("Please provide appropriate input!")
+}
 
-  initFile <- read.table("./../dataRepo/highConfidenceEverything_0.7.txt", sep = "\t", header = T)
+mapString2genesymbol <- function(inputFile, mappingFileLoc, outputFileLoc){
 
-  mappingFile <- read.table("../dataRepo/mappingFile.txt", sep = "\t", header = T)
+  initFile <- read.table(inputFile, sep = "\t", header = T)
+
+  mappingFile <- read.table(mappingFileLoc, sep = "\t", header = T)
   finalFile <- merge(initFile, mappingFile, by.x = "protein1", by.y = "STRING")
   finalFile$GeneName1 <- finalFile$Gene.name
   finalFile <- finalFile[c(12, 1:10)]
@@ -44,5 +32,7 @@ mapString2genesymbol <- function(){
   finalFile$GeneName2 <- finalFile$Gene.name
   finalFile <- finalFile[c(2, 13, 4:11)]
 
-  write.table(finalFile, file = "../dataRepo/generatedfiles/HighConfidenceWithGeneNames.txt", sep = "\t", col.name = T, quote = F, row.names = F)
+  write.table(finalFile, file = outputFileLoc, sep = "\t", col.name = T, quote = F, row.names = F)
 }
+
+mapString2genesymbol(inputFile, mappingFileLoc, outputFileLoc)
